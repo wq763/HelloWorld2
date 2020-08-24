91c891447cf7c2b650a3744633b733ef3351d5c7
wq <wq15249043870@163.com>
2020-08-24 19:58:53 +0800

    Initial commit

---
+++ app/src/main/java/com/bincube/helloworld/HelloWorldActivity.java
@@ -0,0 +1,48 @@
+package com.bincube.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    String msg = "Android: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d(msg,"The onCreat() event");
+      //  Log.d("HelloWorldActivity","onCreat execute");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg,"The onStart() event");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg,"The onResume() event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg,"The onPause() event");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg,"The onStop() event");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(msg,"The onDestory() event");
+    }
+
+}
