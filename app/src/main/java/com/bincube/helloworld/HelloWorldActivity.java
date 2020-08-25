84bf100f22a88906be511ad16cca5a9c743b87c2
wq <wq15249043870@163.com>
2020-08-25 09:42:23 +0800

    HelloWorld2

--- app/src/main/java/com/bincube/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/bincube/helloworld/HelloWorldActivity.java
@@ -8,7 +8,7 @@ public class HelloWorldActivity extends AppCompatActivity {
     String msg = "Android: ";
 
     /**
-    * 修改国会大厦公司dx
+    * 修改国会大厦公司dxvgbnh
     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
